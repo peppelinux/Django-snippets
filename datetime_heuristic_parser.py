04428c93fc1e56f0f321930efdc189fc0873f11c
Giuseppe De Marco <demarcog83@gmail.com>
2019-02-04 14:54:28 +0100

    Update datetime_heuristic_parser.py

--- datetime_heuristic_parser.py
+++ datetime_heuristic_parser.py
@@ -5,8 +5,8 @@
 # python3 -m timeit -s "import datetime" "datetime.datetime.strptime('2019-02-03T17:27:58.645194', '%Y-%m-%dT%H:%M:%S.%f')"
 # 10000 loops, best of 3: 53.5 usec per loop
 
-#python3 -m timeit -s  "import sys, os; sys.path.append(os.getcwd()); from datetime_heuristic_parser import datetime_heuristic_parser; datetime_heuristic_parser('2019-02-03T17:27:58.645194')"
-# 100000000 loops, best of 3: 0.00879 usec per loop
+# python3 -c  "import sys, os; sys.path.append(os.getcwd()); from datetime_heuristic_parser import datetime_heuristic_parser; print(datetime_heuristic_parser('04/12/2018 09:7:4Z'))"
+# 10000000 loops, best of 3: 0.0241 usec per loop
 import datetime
 import re
 import pytz
@@ -114,6 +114,7 @@ def datetime_heuristic_parser(value):
     """
     res = dateformat_insp(value) or \
           datetimeformat_insp(value)
+    if not res: raise Exception(ValueError)
     res_dicts_list = []
     for i in res:
         dt_dict = i[-1]
