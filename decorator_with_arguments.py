1ca6e9867b7295af1d8302c23f050f976c7ec8f3
Giuseppe De Marco <demarcog83@gmail.com>
2019-05-29 12:16:17 +0200

    Create decorator_with_arguments.py

---
+++ decorator_with_arguments.py
@@ -0,0 +1,28 @@
+from django.http.response import HttpResponseRedirect, HttpResponse
+from functools import wraps
+from django.urls import reverse
+
+
+def decorator(arg1, arg2):
+    """an explained example
+    """
+    def inner_function(function):
+        @wraps(function)
+        def wrapper(*args, **kwargs):
+            print("Arguements passed to decorator %s and %s" % (arg1, arg2))
+            function(*args, **kwargs)
+        return wrapper
+    return inner_function
+
+
+def redirect_if_authenticated(redirect_to):
+    def inner_function(function):
+        @wraps(function)
+        def wrapper(*args, **kwargs):
+            request = args[0]
+            print("Arguements passed to decorator {}".format(redirect_to))
+            if request.user.is_authenticated:
+                return HttpResponseRedirect(reverse(redirect_to))
+            function(*args, **kwargs)
+        return wrapper
+    return inner_function
